bfa10550d8d63f00dc39450e58bfb7da2c732e06
Starwort <tcphone93@gmail.com>
2019-02-20 23:54:45 +0000

    Bumped version number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ AsyncTK's window is used as TK's is, it's created, and components are placed ont
 
 setup(
     name="asynctk",
-    version="0.0.2a",
+    version="0.0.3a",
     author='Starwort',
     description="An asynchronous wrapper for TK/TCL using TKInter and AsyncIO",
     long_description=long_desc,
